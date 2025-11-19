e4388c8d77e34ea9b8eb69a0c8bf43931ff58282
Joshua Byrd <phocks@gmail.com>
2025-11-19 11:41:44 +1000

    Remove some

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
 import { Days, isit } from "@phocks/isit";
-import axios from "axios";
 import { to as wrap } from "await-to-js";
 import { parseCsv } from "~/src/lib/papa.ts";
-import { type ParseError, type ParseResult } from "papaparse";
 
 const QUOTES_SPREADSHEET_URL =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQoUhnrBaWZCsG16Fj4Kx7Q4_JC3WP3VokhT9EwpPxlGldvoq-B0jCExV3G0UEteOXes7qAe86E9ZWA/pub?output=csv";
@@ -22,6 +20,7 @@ export function getJoinNowLink(
     "19%3ameeting_YjA0NzcxMTItNzY4NS00MDY4LTk5ZGQtODZkNTQ5MjliOTJj%40thread.v2/" +
     "0?context=%7b%22Tid%22%3a%2297c1409a-7078-47e7-bb94-d1e53503e012%22%2c%22Oid%22%3a%22c13a27fd" +
     "-6b00-48ab-a0ad-d7dfb7667cb0%22%7d|Click to join>";
+
   const notMondayLinkText = "<https://teams.microsoft.com/l/meetup-join/" +
     "19%3ameeting_MjljMTMyMzQtMTc3OC00YjBkLTg0YjEtMmQxOTNkZWVjNjIy%40thread.v2/" +
     "0?context=%7b%22Tid%22%3a%2297c1409a-7078-47e7-bb94-d1e53503e012%22%2c%22Oid%22%3a%22c13a27fd" +
